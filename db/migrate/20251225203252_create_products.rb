2fbbe5008fe061a8768bdb3e1decf8a86d46c53a
vlad-henk <vladsukhenko1539@gmail.com>
2025-12-25 22:37:41 +0200

    scaffold product

---
+++ db/migrate/20251225203252_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.boolean :available
+
+      t.timestamps
+    end
+  end
+end
